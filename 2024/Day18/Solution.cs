138acf91ee7f5ede6035b41989220a00c4342007
vinleclair <vinleclair@gmail.com>
2024-12-18 17:57:24 -0500

    Add Day18

---
+++ 2024/Day18/Solution.cs
@@ -0,0 +1,69 @@
+﻿using System.Numerics;
+using System.Text.RegularExpressions;
+
+namespace AdventOfCode._2024.Day18;
+
+public partial class Solution : ISolution
+{
+    private const int Size = 70;
+    private static readonly Vector2 Up = new(0, -1);
+    private static readonly Vector2 Down = new(0, 1);
+    private static readonly Vector2 Left = new(-1, 0);
+    private static readonly Vector2 Right = new(1, 0);
+
+    public object PartOne(string input) => Dijkstra(ParseInput(input).Take(1024)) ?? int.MinValue;
+
+    public object PartTwo(string input)
+    {
+        var bytes = ParseInput(input).ToArray();
+        
+        var (low, high) = (0, bytes.Length);
+        
+        while (low < high - 1) {
+            var mid = (low + high) / 2;
+            if (Dijkstra(bytes.Take(mid)) == null) {
+                high = mid;
+            } else {
+                low = mid;
+            }
+        }
+
+        return bytes[low];
+    }
+
+    private static int? Dijkstra(IEnumerable<Vector2> bytes)
+    {
+        var (start, end) = (new Vector2(0, 0), new Vector2(Size, Size));
+        var corrupted = bytes.Append(start).ToHashSet();
+
+        var priorityQueue = new PriorityQueue<Vector2, int>();
+        priorityQueue.Enqueue(start, 0);
+
+        while (priorityQueue.TryDequeue(out var position, out var distance))
+        {
+            if (position == end)
+                return distance;
+
+            foreach (var direction in new[] { Up, Down, Left, Right })
+            {
+                var newPosition = position + direction;
+                if (!corrupted.Contains(newPosition) &&
+                    newPosition is { X: >= 0 and <= Size, Y: >= 0 and <= Size })
+                {
+                    priorityQueue.Enqueue(newPosition, distance + 1);
+                    corrupted.Add(newPosition);
+                }
+            }
+        }
+
+        return null;
+    }
+
+    private static IEnumerable<Vector2> ParseInput(string input) => input.Split("\n")
+        .Select(line =>
+            new { line, nums = OneOrMoreDigits().Matches(line).Select(m => int.Parse(m.Value)).ToArray() })
+        .Select(line => new Vector2(line.nums[0], line.nums[1]));
+
+    [GeneratedRegex(@"\d+", RegexOptions.Multiline)]
+    private static partial Regex OneOrMoreDigits();
+}
