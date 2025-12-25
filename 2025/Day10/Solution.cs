1b801691ce44ca94bd5953d7e27ab6652311ba1c
vinleclair <vinleclair@gmail.com>
2025-12-24 21:45:31 -0500

    Add day 10 part 1

--- 2025/Day10/Solution.cs
+++ 2025/Day10/Solution.cs
@@ -1,17 +1,73 @@
 namespace AdventOfCode._2025.Day10;
 
+using Machine = (HashSet<int> lights, int[][] wiring, int[] joltage);
+
 public class Solution : ISolution
 {
-    public object PartOne(string input)
+    public object PartOne(string input) => ParseInput(input).Sum(machine => PushButtons(machine));
+
+    private static int PushButtons(Machine machine, int count = 1)
     {
-        /*
-            ____                            ____ _                            _______  ___ 
-           | __ )  ___  _ __  _ __   ___   / ___| |__   __ _ _ __   ___ ___  |  ___\ \/ / |
-           |  _ \ / _ \| '_ \| '_ \ / _ \ | |   | '_ \ / _` | '_ \ / __/ _ \ | |_   \  /| |
-           | |_) | (_) | | | | | | |  __/ | |___| | | | (_| | | | | (_|  __/ |  _|  /  \|_|
-           |____/ \___/|_| |_|_| |_|\___|  \____|_| |_|\__,_|_| |_|\___\___| |_|   /_/\_(_)
-         */
-        
-        throw new NotImplementedException();
+        if (count == machine.wiring.Length)
+            return count;
+
+        var combinations = GetCombinations(machine.wiring, count);
+
+        foreach (var combo in combinations)
+        {
+            var lights = combo.SelectMany(i => i).GroupBy(x => x)
+                .Where(g => g.Count() % 2 != 0)
+                .Select(g => g.Key)
+                .ToHashSet();
+
+            if (lights.SetEquals(machine.lights))
+                return count;
+        }
+
+        return PushButtons(machine, count + 1);
+    }
+
+    private static IEnumerable<IEnumerable<T>> GetCombinations<T>(T[] items, int length)
+    {
+        if (length == 0)
+        {
+            yield return [];
+            yield break;
+        }
+
+        for (var i = 0; i <= items.Length - length; i++)
+        {
+            foreach (var combo in GetCombinations(items[(i + 1)..], length - 1))
+            {
+                yield return combo.Prepend(items[i]);
+            }
+        }
+    }
+
+    private static Machine[] ParseInput(string input)
+    {
+        var lines = input.Split("\n");
+        var machines = new Machine[lines.Length];
+
+        foreach (var (line, index) in lines.Select((l, i) => (l, i)))
+        {
+            var parts = line.Split(' ');
+
+            var lights = parts[0]
+                .Trim('[', ']')
+                .Index()
+                .Where(x => x.Item == '#')
+                .Select(x => x.Index)
+                .ToHashSet();
+
+            var wiring = parts[1..^1].Select(p => p.Where(char.IsDigit).Select(c => int.Parse(c.ToString())).ToArray())
+                .ToArray();
+
+            var joltage = parts[^1][1..^1].Split(",").Select(int.Parse).ToArray();
+
+            machines[index] = new Machine(lights, wiring, joltage);
+        }
+
+        return machines;
     }
 }
